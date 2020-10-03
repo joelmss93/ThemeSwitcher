375ec4a04714dfdeafbda1eaa76e9332959e495f
joelmss93 <joelmss93@gmail.com>
2020-10-02 21:13:10 -0300

    Enviando Projeto

---
+++ src/styles/themes/dark.ts
@@ -0,0 +1,11 @@
+export default {
+    title: 'dark',
+
+    colors: {
+        primary: '#031927',
+        secundary: '#508AA8',
+
+        background: '#161617',
+        text: '#FFF'
+    },
+}
---
+++ src/styles/themes/light.ts
@@ -0,0 +1,11 @@
+export default {
+    title: 'light',
+
+    colors: {
+        primary: '#031927',
+        secundary: '#508AA8',
+
+        background: '#C8E0F4',
+        text: '#333'
+    },
+}
